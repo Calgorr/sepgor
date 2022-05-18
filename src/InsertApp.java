2779f1556eb8bb6b03d0858d0a8a272a33f306a5
sepehrmnp <sepehr.mirnasrollahi@gmail.com>
2022-05-18 12:21:12 +0430

    01com

---
+++ src/InsertApp.java
@@ -0,0 +1,61 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class InsertApp {
+
+    /**
+     * Connect to the test.db database
+     *
+     * @return the Connection object
+     */
+    private static Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C:\\Users\\NP\\IdeaProjects\\sepgor01\\sepgor01.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    /**
+     * Insert a new row into the warehouses table
+     *
+     * @param name
+     * @param capacity
+     */
+    public static void insert(String name, double capacity) {
+        String sql = "INSERT INTO USERS(userName,passWord) VALUES(?,?,?)";
+
+        try (Connection conn = connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+
+    public static void main(String[] args) {
+
+        InsertApp app = new InsertApp();
+        // insert three new rows
+        app.insert("Raw Materials", 3000);
+        app.insert("Semifinished Goods", 4000);
+        app.insert("Finished Goods", 5000);
+    }
+     */
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+	// write your code here
+       // Connect.connect();
+        System.out.println(HASH.sha3("sepehrmnp"));
+
+    }
+}
