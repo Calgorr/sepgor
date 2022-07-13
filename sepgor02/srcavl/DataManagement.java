1e64fbabd2d5d8c366c418abe1247e9a44100fb6
sepehrmnp <sepehr.mirnasrollahi@aut.ac.ir>
2022-07-13 14:50:54 +0430

    Delete sepgor02 directory

--- sepgor02/srcavl/DataManagement.java
+++
@@ -1,8 +0,0 @@
-public interface DataManagement {
-
-    User getUser(String userName);
-    User getUser(int id);
-    String getPassword(String userName);
-    boolean IdIsValid (int id);
-    int insertSession(User user, String session);
-}
